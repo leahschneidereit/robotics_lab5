be38dbb48b0b1a3772baa7c340ff46356029675f
ls8800 <ls8800@uncw.edu>
2023-03-30 19:18:55 +0000

    updated errors

---
+++ scripts/sphere_fit.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+import rospy
+import numpy as np
+import cv2
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from robot_vision_lectures.msg import XYZarray
+from robot_vision_lectures.msg import SphereParams
+
+xyz = XYZarray()
+sphere_params = SphereParams()
+print(sphere_params)
+computed = False
+
+def solver(data):
+
+	global computed
+	global xyz
+	global sphere_params
+	
+	points = data.points
+	
+	# develop A and B matrices
+	A = []
+	B = []
+	
+	# iterate over points and add to matrices for xyz
+	for i in range (len(points)):
+		if i == 0:
+			print(points[i].x,points[i].y,points[i].z)
+		A2 = []
+		A2.append(2*points[i].x)
+		A2.append(2*points[i].y)
+		A2.append(2*points[i].z)
+		A2.append(1)
+		A.append(A2)
+		#A.append([2*points[i].x, 2*points[i].y, 2*points[i].z, 1])
+		B.append(points[i].x**2 + points[i].y**2 + points[i].z**2)
+	
+	# calculate P
+	P = np.linalg.lstsq(A,B,rcond = None)[0]
+	
+	# calculate radius
+	r = np.sqrt(P[0]**2 + P[1]**2 + P[2]**2 + P[3])
+	print(r)
+	
+	# assign values to sphere data 
+	sphere_params.xc = P[0]
+	sphere_params.yc = P[1]
+	sphere_params.zc = P[2]
+	
+	# set flag to True
+	computed = True
+
+if __name__ == '__main__':
+	# define node
+	rospy.init_node('sphere_fit', anonymous = True)
+	# define a subscriber to read images
+	img_sub = rospy.Subscriber('/xyz_cropped_ball', XYZarray, solver) 
+	# define a publisher to publish images
+	img_pub = rospy.Publisher('sphere_params', SphereParams, queue_size = 1)
+	
+	# set the loop frequency
+	rate = rospy.Rate(10)
+
+	while not rospy.is_shutdown():
+		# publish if data has been calculated 
+		if computed:
+			img_pub.publish(sphere_params)
+			computed = False 
+		# pause until the next iteration			
+		rate.sleep()
